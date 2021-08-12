644b7fb49f51afa4cc554adb6fbd4662dcb52c6c
jerrinot <jaromir.hamala@gmail.com>
2021-08-12 14:33:29 +0200

    Introduce a config option to use a local Hazelcast cluster

--- hockey-stats-app/src/main/java/biz/schr/cdcdemo/Config.java
+++ hockey-stats-app/src/main/java/biz/schr/cdcdemo/Config.java
@@ -15,12 +15,18 @@ public class Config {
     public static final int dbPort = 3306;
     public static final String dbURL = "jdbc:mysql://" + dbIP + ":" + dbPort + "/" + dbName;
 
+    public static final boolean USE_CLOUD = false;
+
     public static ClientConfig newClientConfig() {
-        ClientConfig config = new ClientConfig();
-        config.setProperty(STATISTICS_ENABLED.getName(),"true");
-        config.setProperty(HAZELCAST_CLOUD_DISCOVERY_TOKEN.getName(),"gA6VBZaa7u2w5967oWK0uPFIXpf6t3s2kgmUiANib8xrQglpv9");
-        config.setProperty("hazelcast.client.cloud.url","https://dev.test.hazelcast.cloud");
-        config.setClusterName("de-1818");
-        return config;
+        if (USE_CLOUD) {
+            ClientConfig config = new ClientConfig();
+            config.setProperty(STATISTICS_ENABLED.getName(), "true");
+            config.setProperty(HAZELCAST_CLOUD_DISCOVERY_TOKEN.getName(), "gA6VBZaa7u2w5967oWK0uPFIXpf6t3s2kgmUiANib8xrQglpv9");
+            config.setProperty("hazelcast.client.cloud.url", "https://dev.test.hazelcast.cloud");
+            config.setClusterName("de-1818");
+            return config;
+        } else {
+            return new ClientConfig();
+        }
     }
 }
