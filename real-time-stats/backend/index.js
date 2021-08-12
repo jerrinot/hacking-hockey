f02eaa828248be4dafb9fe196e25d0133951d52f
Serkan Özel <serkan.ozel@hazelcast.com>
2021-08-12 13:05:50 +0300

    Nodejs client connected to the sample output producer

--- real-time-stats/backend/index.js
+++ real-time-stats/backend/index.js
@@ -1,1 +1,24 @@
-console.log('hello');
+'use strict';
+const { Client } = require('hazelcast-client');
+
+(async () => {
+    let client;
+    try {
+        client = await Client.newHazelcastClient();
+        const map = await client.getMap('top_5_map');
+
+        await map.addEntryListener({
+            added: (entryEvent) => {
+                console.log(entryEvent);
+            },
+            updated: (entryEvent) => {
+                console.log(entryEvent);
+            }
+        }, undefined, true);
+
+    } catch (err) {
+        await client.shutdown();
+        console.error('Error occurred:', err);
+        process.exit(1);
+    }
+})();
